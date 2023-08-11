c09af1a0ff3dfb4015b00dfadf20a4ec2a43cef4
Dymirt <kolidadmytro@gmail.com>
2023-08-11 15:13:08 +0200

    adding storisma app
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- integrations/urls.py
+++ integrations/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include("wordpress.urls", namespace='products')),
+    path('storisma/', include('storisma.urls', namespace='storisma')),
 ]
